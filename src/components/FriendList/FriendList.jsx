943db724536941df2ecda834103e51415046a52b
Heldenglanz <heldenglanz@gmail.com>
2022-09-08 18:38:40 +0300

    pr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,30 @@
 import PropTypes from 'prop-types'
 import FriendListItem from './FriendListItem'
+// import s from '../FriendList.module.css'
+function FriendList({ friends }) {
+    return (
+        <ul className="friend-list">
+            {friends.map(friend => {
+                return (
+                    <li className="item" key={friend.id}>
+                        <FriendListItem
+                            avatar={friend.avatar}
+                            name={friend.name}
+                            isOnline={friend.isOnline}
+                        />
+                    </li>
+                )
+            })}
 
-function FriendList({friends}){
-    return(
-    <ul className="friend-list">
-        {friends.map(friend=>{
-            return(
-    <li className="item" key={friend.id}>
-<FriendListItem 
-avatar={friend.avatar}
-name={friend.name}
-isOnline={friend.isOnline}
-/>
-</li>
-)})}
-
-</ul>
-)} 
+        </ul>
+    )
+}
 
 FriendList.propTypes = {
-    friends:PropTypes.arrayOf(
+    friends: PropTypes.arrayOf(
         PropTypes.shape({
-    key:PropTypes.number.isRequired,
-    }).isRequired).isRequired
+            key: PropTypes.number.isRequired,
+        }).isRequired).isRequired
 }
 
 
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 
-function FriendListItem({avatar, name, isOnline}){
-return(
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
     <>
-  <span className={isOnline ? "green":"red"}></span>
-  <img className="avatar" src={avatar} alt="User avatar" width="48" />
-  <p className="name">{name}</p>
-  </>
-)
+      <span className={isOnline ? "green" : "red"}></span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </>
+  )
 }
 
 FriendListItem.propTypes = {
-    avatar:PropTypes.string.isRequired,
-    name:PropTypes.string.isRequired,
-    isOnline:PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 }
 
 
